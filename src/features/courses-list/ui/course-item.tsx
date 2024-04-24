3f31b3ef786140d0b09ec0ec5411c983c3f5bdce
OzevaS <roma_avezov@mail.ru>
2024-04-24 23:53:28 +0300

    chore: add basic ui exmaple

---
+++ src/features/courses-list/ui/course-item.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "@/shared/ui/card";
+import { CourseListElement } from "../model/types";
+import { Button } from "@/shared/ui/button";
+import { useTransition } from "react";
+
+export function CourseItem({course, onDelete}: {course: CourseListElement; onDelete: () => Promise<void>}) {
+
+    const [isLoadingDelete, startDeleteTransition] = useTransition()
+
+    const handleDelete = () => {
+        startDeleteTransition(async () => {
+            await onDelete()
+        })
+    }
+
+    return (
+        <Card>
+        <CardHeader>
+          <CardTitle>{course.name}</CardTitle>
+          <CardDescription>{course.description}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Button disabled={isLoadingDelete} onClick={handleDelete}>Удалить</Button>
+        </CardFooter>
+      </Card> 
+    )
+}
