26bff140c9b24d5f68cf5f2de1feea766695c4c4
OzevaS <roma_avezov@mail.ru>
2024-05-03 00:46:14 +0300

    feat: add email auth

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -2,8 +2,22 @@ import { AuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { dbClient } from "@/shared/lib/db";
 import { compact } from "lodash-es";
+import EmailProvider from "next-auth/providers/email";
+import { privateConfig } from "@/shared/config/private";
 
 export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient) as AuthOptions["adapter"],
-  providers: compact([]),
+  providers: compact([
+    EmailProvider({
+      server: {
+        host: privateConfig.EMAIL_SERVER_HOST,
+        port: Number(privateConfig.EMAIL_SERVER_PORT),
+        auth: {
+          user: privateConfig.EMAIL_SERVER_USER,
+          pass: privateConfig.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: privateConfig.EMAIL_FROM,
+    }),
+  ]),
 };
