cbaf851fc87d433ab01a1cbda37bc0b0a7f02a11
OzevaS <roma_avezov@mail.ru>
2024-05-06 23:08:50 +0300

    fix: s3 config region and tags

--- src/shared/lib/file-storage.ts
+++ src/shared/lib/file-storage.ts
@@ -1,4 +1,4 @@
-import { S3Client, Tag } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 import cuid from "cuid";
 import { privateConfig } from "../config/private";
@@ -16,31 +16,26 @@ class FileStorage {
   private s3Client = new S3Client({
     forcePathStyle: true,
     endpoint: privateConfig.S3_ENDPOINT,
-    region: "eu-west-1",
+    region: privateConfig.S3_REGION,
     credentials: {
       accessKeyId: privateConfig.S3_ACCESS_KEY_ID,
       secretAccessKey: privateConfig.S3_SECRET_ACCESS_KEY,
     },
   });
 
-  async uploadImage(file: File, options?: { tags?: Tag[] }) {
-    return this.upload(file, privateConfig.S3_IMAGES_BUCKET, options);
+  async uploadImage(file: File, tag: string) {
+    return this.upload(file, privateConfig.S3_IMAGES_BUCKET, tag);
   }
 
-  async upload(
-    file: File,
-    bucket: string,
-    options?: { tags?: Tag[] },
-  ): Promise<StoredFile> {
+  async upload(file: File, bucket: string, tag: string): Promise<StoredFile> {
     const res = await new Upload({
       client: this.s3Client,
       params: {
         ACL: "public-read",
         Bucket: bucket,
-        Key: `${Date.now().toString()}-${file.name}`,
+        Key: `${tag}-${Date.now().toString()}-${file.name}`,
         Body: file,
       },
-      tags: options?.tags ?? [],
       queueSize: 4, // optional concurrency configuration
       partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
       leavePartsOnError: false, // optional manually handle dropped parts
